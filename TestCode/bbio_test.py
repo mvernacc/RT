12ab7d88c3ff9dbb0569fd402464b709336af5d6
Norman Cao <maplenormandy@gmail.com>
2013-02-23 17:16:15 -0500

    Removed async stuff

--- TestCode/bbio_test.py
+++ TestCode/bbio_test.py
@@ -9,50 +9,35 @@ POT = AIN5
 brightness = 0  # Global variable to store brightness level
 inc = 1         # How much to increment the brightness by
 
+frame = 0
+
 # Create a setup function:
 def setup():
   # Start the serial port at 9600 baud:
   Serial2.begin(9600)
-
-  # Add threads
-  t = threading.Thread(target=pwm_loop)
-  t.daemon = True
-  t.start()
-  t = threading.Thread(target=analog_loop)
-  t.daemon = True
-  t.start()
-  t = threading.Thread(target=serial_loop)
-  t.daemon = True
-  t.start()
-
-def pwm_loop():
-  global brightness, inc
-
-  while True:
-    # Set the PWM duty cycle:
-    analogWrite(LED, brightness)
-    # Increment value:
-    brightness += inc
-    if ((brightness == 255) or (brightness == 0)):
-      # Change increment direction:
-      inc *= -1
-    # Sleep a bit:
-    delay(10)
   
-def analog_loop():
-  while True:
+def loop():
+  global frame
+
+  frame += 1
+
+  # Set the PWM duty cycle:
+  analogWrite(LED, brightness)
+  # Increment value:
+  brightness += inc
+  if ((brightness == 255) or (brightness == 0)):
+    # Change increment direction:
+    inc *= -1
+
+  if frame%50 == 0:
     val = analogRead(POT)
     voltage = inVolts(val)
     print " ADC value: %i - voltage: %0.2f" % (val, voltage)
-    delay(500)
 
-def serial_loop():
-  while True:
+  if frame%3 == 0:
     Serial2.write(random.randint(0,255))
-    delay(25)
-
-def loop():
-  delay(30)
+  
+  delay(10)
 
 # Start the loop:
 run(setup, loop)
