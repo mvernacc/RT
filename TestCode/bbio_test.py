4a2814ba7bedf465c703bdd13bec432b78cdc921
Norman Cao <maplenormandy@gmail.com>
2013-02-23 16:17:57 -0500

    More random bugfixes (no pun intended

--- TestCode/bbio_test.py
+++ TestCode/bbio_test.py
@@ -28,25 +28,28 @@ def setup():
 def pwm_loop():
   global brightness, inc
 
-  # Set the PWM duty cycle:
-  analogWrite(LED, brightness)
-  # Increment value:
-  brightness += inc
-  if ((brightness == 255) or (brightness == 0)):
-    # Change increment direction:
-    inc *= -1
-  # Sleep a bit:
-  delay(pause)
+  while True:
+    # Set the PWM duty cycle:
+    analogWrite(LED, brightness)
+    # Increment value:
+    brightness += inc
+    if ((brightness == 255) or (brightness == 0)):
+      # Change increment direction:
+      inc *= -1
+    # Sleep a bit:
+    delay(pause)
   
 def analog_loop():
-  val = analogRead(POT)
-  voltage = inVolts(val)
-  print " ADC value: %i - voltage: %0.2f" % (val, voltage)
-  delay(500)
+  while True:
+    val = analogRead(POT)
+    voltage = inVolts(val)
+    print " ADC value: %i - voltage: %0.2f" % (val, voltage)
+    delay(500)
 
 def serial_loop():
-  Serial2.write(randint(0,255))
-  delay(25)
+  while True:
+    Serial2.write(random.randint(0,255))
+    delay(25)
 
 def loop():
   delay(30)
