5a4cfffcc703426c0f48984b058a0f89d14dd441
Norman Cao <maplenormandy@gmail.com>
2013-02-23 17:25:10 -0500

    Added comments and junk

--- TestCode/bbio_test.py
+++ TestCode/bbio_test.py
@@ -1,3 +1,6 @@
+# Combined examples taken from https://github.com/alexanderhiam/PyBBIO
+# I already installed the python module. Simply run sudo python bbio_test.py
+
 # Import PyBBIO library:
 from bbio import *
 import threading
@@ -29,15 +32,32 @@ def loop():
     # Change increment direction:
     inc *= -1
 
+  # Run analog reading once every 500 ms
   if frame%50 == 0:
     val = analogRead(POT)
     voltage = inVolts(val)
     print " ADC value: %i - voltage: %0.2f" % (val, voltage)
 
+  # Run serial writing once every 30 ms
   if frame%3 == 0:
     Serial2.write(random.randint(0,255))
   
   delay(10)
 
 # Start the loop:
-run(setup, loop)
+#run(setup, loop) <-- this is the library's normal way of doing startup
+
+# This is the code that the library runs. If we want to have our own control
+# loop, then we can probably play around with this.
+try:
+  bbio_init()
+  setup()
+  while (True):
+    loop()
+except KeyboardInterrupt:
+  # Manual exit signal, clean up and exit happy
+  bbio_cleanup()
+except Exception, e:
+  # Something may have gone wrong, clean up and re-raise exception
+  bbio_cleanup()
+  raise e
